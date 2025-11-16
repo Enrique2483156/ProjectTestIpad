ae50877d426dec39cc9fcd9ec1a40c315643b7b8
Enrique2483156 <144407799+Enrique2483156@users.noreply.github.com>
2025-11-16 09:31:06 -0500

    mando mis cambios

--- firstfile.py
+++ firstfile.py
@@ -7,9 +7,14 @@ z = 3,4,5
 #comentario de prueba en python
 variable = f'esta {firstname}  es una {lastname.title().rstrip().lstrip()} \n\t! variaable de python'
 
+mivariable2 = f'esta es{firstname} que veo un texto muy wow'
+
+
 print(variable.title())
 print(variable.lower())
 print(variable.upper())
 
 print(10 ** 2)
+
+print("claro que si es mejor")
 
